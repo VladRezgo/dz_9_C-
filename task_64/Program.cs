94e8cdbb0d7bb9b1c1ed39a65ee6851f1175fe73
«Vlad» <vladislavrezgo@gmail.com>
2022-07-27 18:37:45 -0800

    task64

---
+++ task_64/Program.cs
@@ -0,0 +1,22 @@
+﻿ 
+  //задание 64. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+ Console.Write("введите первое число ");
+ int a = Convert.ToInt32(Console.ReadLine());
+  Console.Write("введите второе число ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ 
+  
+ string NumInterges (int a, int n)
+{
+    if (a == n) 
+    {
+        return n.ToString(); 
+    }
+    else 
+    {
+        return a.ToString() + " " + NumInterges(a+1 , n); 
+    }
+}
+
+
+Console.Write("вcе натуральные числа в заданном диапазоне => "+ NumInterges(a,n));
